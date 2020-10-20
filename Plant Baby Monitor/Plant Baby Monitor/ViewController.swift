1d4efab76aa065d913c0d1b5db591fada71c2593
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2020-10-20 17:31:58 +1100

    Added initial files and created the project in xcode

---
+++ Plant Baby Monitor/Plant Baby Monitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plant Baby Monitor
+//
+//  Created by Farhad Ullah Rezwan on 20/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kanban.md
@@ -0,0 +1,14 @@
+## To Do
+
+- User Authentication and login
+    @ 2020-10-21
+    > setup user authtication using Firebase
+    * [ ] Login
+    * [ ] Register
+    * [ ] UI of Storyboards minimal
+
+## Doing
+
+
+## Done
+
